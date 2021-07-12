4cbea6536ea61913f2e2b93c325b2987b3cc9cbf
Oleksandr Ivashchuk <67271891+Lookout84@users.noreply.github.com>
2021-07-12 21:44:23 +0300

    fix repeat password

--- src/model/user.js
+++ src/model/user.js
@@ -62,7 +62,7 @@ userSchema.pre("save", async function (next) {
   if (this.isModified("password")) {
     const salt = await bcrypt.genSalt(SALT_WORK_FACTOR);
     this.password = await bcrypt.hash(this.password, salt);
-    this.confirmPassword = await bcrypt.hash(this.confirmPassword, salt);
+    this.repeatPassword = await bcrypt.hash(this.repeatPassword, salt);
   }
   next();
 });
